9e1098b1ebde357e80919995e6f6ca0e85286a5c
ElectricSheep68 <uramitokei68@gmail.com>
2014-11-08 22:10:57 +0900

    敵の生成、生成した敵の消滅

--- Assets/Code/Script/EnemyGenerator.cs
+++ Assets/Code/Script/EnemyGenerator.cs
@@ -2,13 +2,39 @@
 using System.Collections;
 
 public class EnemyGenerator : MonoBehaviour {
-	//enemyの定義
-
-	void Update () {
-		//enemyが時間ごとに生成
-
-		//時間を蓄える
-		//float accum = 
+	//プレハブを定義
+	public GameObject prefab;
+	public float randum = 7f;
+	public float enemySpeed = 10f;
+	public float enemySpan = 10f;
+	float accum = 0;
 	
+	void Update() {
+		//経過時間を保存
+		accum += Time.deltaTime;
+
+		
+		if (accum >= enemySpan) 
+		{
+			Generate ();
+			accum = 0;
+		}
+	}
+	
+	
+	//敵を生成する関数を新しく作る
+	void Generate()
+	{
+		
+		//ランダムな位置を定義 この~~~;はvector3
+		Vector3 randamuPos = transform.position + new Vector3(Random.Range(-randum,randum), 0, 0);//現在地から
+		//Randomを使うときはランダム数値を使う前に触れ幅を見ておく。
+		//Debug.Log(randamuPos);
+		//プレハブを生成
+		GameObject enemy = (GameObject)Instantiate(prefab, randamuPos, transform.rotation);
+		//前に打ち出す 力には種類が４つあり。Impulse Forceはよく使う
+		//。違いは、Forceが毎フレーム動かす(FixedUpdateとかで)、impulseが単発
+		
+		enemy.rigidbody.AddForce(transform.forward*enemySpeed,ForceMode.Impulse);
 	}
 }
