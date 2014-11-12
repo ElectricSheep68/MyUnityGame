8f4555a3795fb68a5763b3d4440a9393c9ea9e63
ElectricSheep68 <uramitokei68@gmail.com>
2014-11-13 00:11:41 +0900

    エラー修正

--- Assets/Code/AI/PlayerController.cs
+++ Assets/Code/AI/PlayerController.cs
@@ -2,91 +2,65 @@
 using System.Collections;
 namespace Saiyaku{
 public class PlayerController : MonoBehaviour 
-{
-	public GameObject Bullet;
-	
-	private Transform Turret;
-	private Transform bulletSpawnPoint;    
-	private float curSpeed, targetSpeed, rotSpeed;
-	private float turretRotSpeed = 10.0f;
-	private float maxForwardSpeed = 300.0f;
-	private float maxBackwardSpeed = -300.0f;
-	
-	protected float shootRate;
-	protected float elapsedTime;
-	
-	void Start()
 	{
-		rotSpeed = 150.0f;
+		public float speed = 5.0f;
+		public float mass = 5.0f;
+		public float force = 1.0f;
+		public float minimumDistToAvoid = 1.0f;
 		
-		Turret = gameObject.transform.GetChild(0).transform;
-		bulletSpawnPoint = Turret.GetChild(0).transform;
-	}
-	
-	void OnEndGame()
-	{
-		this.enabled = false;
-	}
-	
-	void Update()
-	{
-		UpdateControl();
-		UpdateWeapon();
-	}
-	
-	void UpdateControl()
-	{
-		Plane playerPlane = new Plane(Vector3.up, transform.position + new Vector3(0, 0, 0));
+		//Actual speed of the vehicle 
+		private float curSpeed;
+		private Vector3 targetPoint;
 		
-		Ray RayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
-		
-		float HitDist = 0;
-		
-		if (playerPlane.Raycast(RayCast, out HitDist))
+		// Use this for initialization
+		void Start () 
 		{
-			Vector3 RayHitPoint = RayCast.GetPoint(HitDist);
+			mass = 5.0f;        
+			targetPoint = Vector3.zero;
+		}
+		// Update is called once per frame
+		void Update () 
+		{
 			
-			Quaternion targetRotation = Quaternion.LookRotation(RayHitPoint - transform.position);
-			Turret.transform.rotation = Quaternion.Slerp(Turret.transform.rotation, targetRotation, Time.deltaTime * turretRotSpeed);
-		}
-		
-		if (Input.GetKey(KeyCode.W))
-		{
-			targetSpeed = maxForwardSpeed;
-		}
-		else if (Input.GetKey(KeyCode.S))
-		{
-			targetSpeed = maxBackwardSpeed;
-		}
-		else
-		{
-			targetSpeed = 0;
-		}
-		
-		if (Input.GetKey(KeyCode.A))
-		{
-			transform.Rotate(0, -rotSpeed * Time.deltaTime, 0.0f);
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			transform.Rotate(0, rotSpeed * Time.deltaTime, 0.0f);
-		}
-		
-		curSpeed = Mathf.Lerp(curSpeed, targetSpeed, 7.0f * Time.deltaTime);
-		transform.Translate(Vector3.forward * Time.deltaTime * curSpeed);    
-	}
-	
-	void UpdateWeapon()
-	{
-		if(Input.GetMouseButtonDown(0))
-		{
-			if (elapsedTime >= shootRate)
+			RaycastHit hit;
+			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			
+			if(Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit, 50.0f))
 			{
-				elapsedTime = 0.0f;
-				
-				Instantiate(Bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+				targetPoint = hit.point;
 			}
-		}
+			
+			//目標点への方向ベクトル
+			Vector3 dir = (targetPoint - transform.position);
+			dir.Normalize();
+
+			//目標点に到達後は停止
+			if(Vector3.Distance(targetPoint, transform.position) < 2.5f)
+				return;
+			
+			//速度をデルタタイムで標準化します
+			curSpeed = speed * Time.deltaTime;
+			
+			//操作対象の向きを変えます（回転）
+			var rot = Quaternion.LookRotation(dir);
+			rot.z = 0;
+			rot.x = 0;
+			transform.rotation = Quaternion.Slerp(transform.rotation, rot, 50.0f *  Time.deltaTime);
+			
+			//操作対象を動かします
+			transform.position += transform.forward * curSpeed;
+
+			//敵にぶつかったら敵をぶっ飛ばします
+			}
+			void OnCollisionEnter(Collision collision){
+				if(collision.collider.tag == "Enemy"){
+				Vector3 enemypos = collision.gameObject.transform.position;
+				Vector3 playerpos = transform.position;
+				Vector3 direction = playerpos - enemypos;
+				
+
+				}
+		
+			}
 	}
-}
 }
