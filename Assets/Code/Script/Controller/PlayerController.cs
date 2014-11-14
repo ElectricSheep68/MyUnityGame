24e68197d8621a1181d127c8e9278f73d9709945
アセナエウム <school@athenaeum-society.com>
2014-11-14 14:39:07 +0900

    Unity Test Toolsの導入、単体テスト用にPlayerCtrlの編集

---
+++ Assets/Code/Script/Controller/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+namespace Saiyaku{
+
+	[Serializable]
+	public class PlayerController
+	{
+		public IPlayerCtrl playerController;
+		
+		public PlayerController (){
+		}
+		
+		public void SetCameraMoverController(IPlayerCtrl playerController) {
+			this.playerController = playerController;
+		}
+	}
+}
