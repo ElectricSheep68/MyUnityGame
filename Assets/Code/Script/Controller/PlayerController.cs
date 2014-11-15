083a141f0e03b3c3eed59c343b0e8bce77d4aa54
ElectricSheep68 <uramitokei68@gmail.com>
2014-11-15 13:28:57 +0900

    コントローラー追加

---
+++ Assets/Code/Script/Controller/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+namespace Saiyaku{
+
+	[Serializable]
+	public class PlayerController
+	{
+		private float rotSpeed;
+
+		public IPlayerCtrl playerController;
+		
+		public PlayerController (){
+		}
+		
+		public void SetPlayerController(IPlayerCtrl playerController) {
+			this.playerController = playerController;
+		}
+
+		public float setRotSpeed(float rotSpeed) {
+			this.rotSpeed = rotSpeed;
+		}
+
+		public float getRotSpeed() {
+			return rotSpeed;
+		}
+	}
+}
---
+++ Assets/Code/Script/PlayerCtrl.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using System.Collections;
+namespace Saiyaku{
+	
+	public class PlayerCtrl : MonoBehaviour, IPlayerCtrl
+	{
+		public GameObject Bullet;
+		
+		private Transform Turret;
+		private Transform bulletSpawnPoint;    
+
+		private float curSpeed, targetSpeed;
+		private float turretRotSpeed = 10.0f;
+		private float maxForwardSpeed = 300.0f;
+		private float maxBackwardSpeed = -300.0f;
+
+		protected float shootRate;
+		protected float elapsedTime;
+
+		public PlayerController controller;
+		
+		public void OnEnable() {
+			controller.SetPlayerController (this);
+		}
+
+		void Start()
+		{
+			controller.setRotSpeed(150.0f);
+			
+			SetBulletSpawnPoint ();
+		}
+		
+		void OnEndGame()
+		{
+			this.enabled = false;
+		}
+		
+		void Update()
+		{
+			UpdateControl();
+			UpdateWeapon();
+		}
+		
+		void UpdateControl()
+		{
+			Plane playerPlane = new Plane(Vector3.up, transform.position + new Vector3(0, 0, 0));
+			
+			Ray RayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+			float HitDist = 0;
+			
+			if (playerPlane.Raycast(RayCast, out HitDist))
+			{
+				Vector3 RayHitPoint = RayCast.GetPoint(HitDist);
+				
+				Quaternion targetRotation = Quaternion.LookRotation(RayHitPoint - transform.position);
+				Turret.transform.rotation = Quaternion.Slerp(Turret.transform.rotation, targetRotation, Time.deltaTime * turretRotSpeed);
+			}
+			
+			if (Input.GetKey(KeyCode.W))
+			{
+				targetSpeed = maxForwardSpeed;
+			}
+			else if (Input.GetKey(KeyCode.S))
+			{
+				targetSpeed = maxBackwardSpeed;
+			}
+			else
+			{
+				targetSpeed = 0;
+			}
+			
+			if (Input.GetKey(KeyCode.A))
+			{
+				transform.Rotate(0, -rotSpeed * Time.deltaTime, 0.0f);
+			}
+			else if (Input.GetKey(KeyCode.D))
+			{
+				transform.Rotate(0, rotSpeed * Time.deltaTime, 0.0f);
+			}
+			
+			curSpeed = Mathf.Lerp(curSpeed, targetSpeed, 7.0f * Time.deltaTime);
+			transform.Translate(Vector3.forward * Time.deltaTime * curSpeed);    
+		}
+		
+		void UpdateWeapon()
+		{
+			if(Input.GetMouseButtonDown(0))
+			{
+				if (elapsedTime >= shootRate)
+				{
+					elapsedTime = 0.0f;
+					
+					Instantiate(Bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+				}
+			}
+		}
+
+		void SetBulletSpawnPoint() {
+			Turret = gameObject.transform.GetChild(0).transform;
+			bulletSpawnPoint = Turret.GetChild(0).transform;
+		}
+	}
+}
